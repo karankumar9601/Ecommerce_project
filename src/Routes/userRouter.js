4d1aa126ee97ab10ca5adc0f380a341c05cd8b1b
karan kumar <karan@gmail.com>
2026-03-24 21:12:14 +0530

    i some updated in userRouter and productDetails

--- src/Routes/userRouter.js
+++ src/Routes/userRouter.js
@@ -3,8 +3,9 @@ const express=require("express")
 const userRouter=express.Router();
 const getAllUser=require("../Controllers/userDetails/userOperation")
 const adminMiddleware=require("../Middleware/adminMiddleWareAuth")
-
+const {viewProduct} =require("../Controllers/productData/productDetails")
 //all user routes
 userRouter.get('/user/getAllUser',adminMiddleware,getAllUser)
+userRouter.get('/getAllProduct',viewProduct)
 
 module.exports=userRouter;
